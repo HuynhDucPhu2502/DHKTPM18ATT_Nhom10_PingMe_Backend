63295a5168198a79b50b3dab602ce48878e88d3b
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2025-08-11 23:01:00 +0700

    ws security

---
+++ src/main/java/me/huynhducphu/PingMe_Backend/config/websocket/UserSocketPrincipal.java
@@ -0,0 +1,25 @@
+package me.huynhducphu.PingMe_Backend.config.websocket;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.security.Principal;
+
+/**
+ * Admin 8/11/2025
+ **/
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UserSocketPrincipal implements Principal {
+
+    private String email;
+    private String name;
+
+    @Override
+    public String getName() {
+        return email;
+    }
+
+}
---
+++ src/main/java/me/huynhducphu/PingMe_Backend/config/websocket/WebsocketSecurity.java
@@ -0,0 +1,27 @@
+package me.huynhducphu.PingMe_Backend.config.websocket;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationProvider;
+
+/**
+ * Admin 8/11/2025
+ **/
+@Configuration
+public class WebsocketSecurity {
+
+    @Bean("wsAuthManager")
+    public AuthenticationManager wsAuthManager(
+            JwtAuthenticationConverter jwtAuthenticationConverter,
+            JwtDecoder jwtDecoder
+    ) {
+        var provider = new JwtAuthenticationProvider(jwtDecoder);
+        provider.setJwtAuthenticationConverter(jwtAuthenticationConverter);
+        return new ProviderManager(provider);
+    }
+
+}
