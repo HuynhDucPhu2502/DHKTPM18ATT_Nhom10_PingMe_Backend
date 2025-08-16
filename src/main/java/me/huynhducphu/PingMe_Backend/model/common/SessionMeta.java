d1cf23d424984fa5f91a64a2b397676bf9047bdd
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2025-08-16 23:22:40 +0700

    add spring cache, spring redis

---
+++ src/main/java/me/huynhducphu/PingMe_Backend/model/common/SessionMeta.java
@@ -0,0 +1,17 @@
+package me.huynhducphu.PingMe_Backend.model.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Admin 8/16/2025
+ **/
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class SessionMeta {
+
+    private String test;
+
+}
