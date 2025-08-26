ce65bf77f492e82b69495d66ab4681077e5773dd
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2025-08-26 17:27:57 +0700

    add query method

--- src/main/java/me/huynhducphu/PingMe_Backend/repository/RoomRepository.java
+++ src/main/java/me/huynhducphu/PingMe_Backend/repository/RoomRepository.java
@@ -1,7 +1,11 @@
 package me.huynhducphu.PingMe_Backend.repository;
 
 import me.huynhducphu.PingMe_Backend.model.Room;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -13,4 +17,12 @@ import java.util.Optional;
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Long> {
     Optional<Room> findByDirectKey(String directKey);
+
+    @Query("""
+              select r from Room r
+              join RoomParticipant rp on rp.room = r
+              where rp.id.userId = :userId
+              order by r.lastMessageAt desc nulls last
+            """)
+    Page<Room> findAllByMember(@Param("userId") Long userId, Pageable pageable);
 }
